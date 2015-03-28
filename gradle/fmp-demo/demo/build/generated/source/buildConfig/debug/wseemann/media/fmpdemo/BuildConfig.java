5be030b93b83cef6476e59f9d2a29ed490f0246b
Seemann <seemann@AITSUM1SEEMANN.hsd1.il.comcast.net>
2015-03-28 13:01:08 -0500

    Improved gradle support

--- gradle/fmp-demo/demo/build/generated/source/buildConfig/debug/wseemann/media/fmpdemo/BuildConfig.java
+++ gradle/fmp-demo/demo/build/generated/source/buildConfig/debug/wseemann/media/fmpdemo/BuildConfig.java
@@ -5,9 +5,9 @@ package wseemann.media.fmpdemo;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "wseemann.media.demo";
+  public static final String APPLICATION_ID = "wseemann.media.fmpdemo";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 10;
-  public static final String VERSION_NAME = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
 }
