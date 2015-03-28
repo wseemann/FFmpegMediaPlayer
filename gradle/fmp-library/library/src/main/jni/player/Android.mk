d2118537d08d37ca25a72ae6c808ecf6ed38d226
Seemann <seemann@AITSUM1SEEMANN.hsd1.il.comcast.net>
2015-03-28 13:09:59 -0500

    Fixed ndk build

--- gradle/fmp-library/library/src/main/jni/player/Android.mk
+++ gradle/fmp-library/library/src/main/jni/player/Android.mk
@@ -7,7 +7,7 @@ LOCAL_CFLAGS :=
 LOCAL_SRC_FILES := wseemann_media_MediaPlayer.cpp \
 		mediaplayer.cpp \
 		ffmpeg_mediaplayer.c \
-                #player.c
+                player.c
 LOCAL_SHARED_LIBRARIES := libswresample libavcodec libavformat libavutil
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../ffmpeg/ffmpeg/$(TARGET_ARCH_ABI)/include
 # for native audio
